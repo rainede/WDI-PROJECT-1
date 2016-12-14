5e1b08d5faa7152981131d632f478ca0efb023d3
rainede <rainey@techgiver.com>
2016-12-14 10:48:25 +0000

    add levels

--- js/app.js
+++ js/app.js
@@ -1,19 +1,21 @@
 var Game = Game || {};
 
-
 Game.setupGame = function() {
   this.width          = 400;
   this.gridBase       = 4;
-  // this.pause          = 500;
   this.pause          = 500;
   this.turnCount      = 0;
   this.level          = 2;
+  this.timeUp         = false;
+  this.lastSquare     = 0;
+  this.beenSeen  = [];
 
   this.score          = document.getElementsByClassName('score')[0];
+  this.showLevel      = document.getElementsByClassName('level')[0];
   this.lives          = document.getElementsByClassName('lives')[0];
   this.header         = document.getElementsByTagName('header')[0];
   this.main           = document.getElementsByTagName('main')[0];
-  this.timeSpent      = document.getElementsByClassName('time');
+  this.timer          = document.getElementsByClassName('timer');
 
   this.message           = document.createElement('h2');
   this.message.innerHTML = 'Get ready to play!';
@@ -40,7 +42,6 @@ Game.createGrid = function createGrid() {
   for (i = 0; i < this.gridBase*this.gridBase; i++) {
     square              = document.createElement('li');
     width               = this.width / this.gridBase;
-    console.log(units);
     square.style.width      = width + units;
     square.style.height     = width + units;
     square.style.lineHeight = width + units;
@@ -54,11 +55,13 @@ Game.createGrid = function createGrid() {
 
 Game.play = function play() {
   this.displayInstructions();
+
   setInterval(this.runSequence.bind(this), 500);
 };
 
 Game.displayInstructions = function displayInstructions() {
-  this.message.innerHTML = parseInt(this.level-1)+'. ' +'Choose all of the numbers divisible by ' + this.level;
+  this.message.innerHTML = parseInt(this.level-1)+'. ' +'Choose all of the numbers divisible by ' + this.level + '.'+ '\n\n' +'Get 10 right to move up to the next level!';
+  this.showLevel.innerHTML = this.level-1;
   // this.message.innerHTML += 'Level' + parseInt(this.level-1) +'Choose all of the numbers divisible by ' + this.level +'\n';
 };
 
@@ -74,36 +77,38 @@ Game.getRandomInt = function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-Game.runSequence = function() {
-  var sequenceNumber = this.getRandomInt(0, (this.gridBase*this.gridBase-1));
-  var square         = this.squares[sequenceNumber];
-
-  if (square.innerHTML !== '') return this.runSequence();
-
-  setTimeout(function(){
-    Game.showSquare(square);
-  }, this.pause);
-};
-
-
 
 Game.runSequence = function() {
   var sequenceNumber = this.getRandomInt(0, (this.gridBase*this.gridBase-1));
   var square         = this.squares[sequenceNumber];
+  var newValue      = this.randomNumber();
+  var bContinue      = false;
 
-  if (square.innerHTML !== '') return this.runSequence();
-
+  switch (true) {
+    case sequenceNumber === this.lastSquare:
+    //don't want to see something popping up in immediately in the same spot
+      break;
+    case square.innerHTML !== '':
+      break;
+    default:
+      if(this.beenSeen.indexOf(newValue)=== -1){
+        // not already seen
+        this.beenSeen.push(newValue);
+        bContinue =true;
+      }
+  }
+  if (!bContinue) return this.runSequence;
   setTimeout(function(){
-    Game.showSquare(square);
+    Game.showSquare(square,newValue);
   }, this.pause * this.getRandomInt(1, 5));
 };
 
 
-Game.showSquare = function showSquare(square){
-  square.innerHTML = this.randomNumber();
+Game.showSquare = function showSquare(square, squareValue){
+  square.style.color = 'white';
+  square.innerHTML = squareValue;
   setTimeout(function(){
     square.innerHTML = '';
-    square.style.color = 'white';
   }, this.pause * this.getRandomInt(1, 5));
 };
 
@@ -113,6 +118,7 @@ Game.selectSquare = function selectSquare() {
     Game.score.innerHTML = parseInt(Game.score.innerHTML) + 1;
   } else {
     this.style.color = 'red';
+    console.log(Game.lives.innerHTML);
     Game.lives.innerHTML = parseInt(Game.lives.innerHTML) - 1;
   }
 
@@ -123,6 +129,8 @@ Game.selectSquare = function selectSquare() {
 
   if (parseInt(Game.score.innerHTML) % 10 === 0) {
     Game.level++;
+    //reset the array
+    Game.beenSeen.splice(0,  Game.beenSeen .length);
     Game.displayInstructions();
   }
 };
